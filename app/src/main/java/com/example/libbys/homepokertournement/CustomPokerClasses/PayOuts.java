b3a184c7e074faeaa5d76a0b978cd54834baf7a7
Joel LIbby <joelmlibby@gmail.com>
2018-11-18 10:53:32 -0500

    Fixed bug in adding players to tournament, fixed warnings in PlayerListActivity. Added payouts for head to head and less than 10.

--- app/src/main/java/com/example/libbys/homepokertournement/CustomPokerClasses/PayOuts.java
+++ app/src/main/java/com/example/libbys/homepokertournement/CustomPokerClasses/PayOuts.java
@@ -1,5 +1,6 @@
 package com.example.libbys.homepokertournement.CustomPokerClasses;
 
+import java.sql.CallableStatement;
 import java.util.ArrayList;
 
 /**
@@ -7,6 +8,10 @@ import java.util.ArrayList;
  */
 
 public class PayOuts {
+    //Applied to head to head tournaments
+    private static final double[] HEADTOHEAD = {1};
+    // Applied to tournaments with less than 3-9 players
+    private static final double[] LESSTHAN10 = {.67, .33};
     //Top 3 players will receive prizes 1st 50% second 30%, and 3rd 20% will be applied to tournaments with less than 30 players
     private static final double[] LESSTHAN30 = {.5, .30, .20};
     //applied to tournaments with 30-50
@@ -16,10 +21,12 @@ public class PayOuts {
     //TODO implement payout schedules for more than 100 tournaments, currently will keep same payouts as 100
 
     public static double[] getPayoutPercentages(int numberofPlayers) {
-        ArrayList<Double> returnVal = new ArrayList<>();
-        if (numberofPlayers < 30) return LESSTHAN30;
+        if (numberofPlayers < 2) return new double[] {};
+        else if (numberofPlayers == 2) return HEADTOHEAD;
+        else if (numberofPlayers < 10) return LESSTHAN10;
+        else if (numberofPlayers < 30) return LESSTHAN30;
         else if (numberofPlayers < 50) return LESSTHAN50;
-        else return LESSTHAN100;
+        return LESSTHAN100;
     }
 
 }
